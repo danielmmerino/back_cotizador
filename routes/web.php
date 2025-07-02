9ff349c6e5ae676f794ba83d7e60bc6ac907604c
danielmmerino <danielm_merino@hotmail.com>
2025-07-02 18:16:30 -0500

    Add ChatGPT connection API

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\VehicleInfoController;
+use App\Http\Controllers\ChatGptController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,3 +15,8 @@ Route::post('/api/login', [AuthController::class, 'login'])
     ]);
 
 Route::middleware('jwt')->get('/api/vehicle-info/{plate}', [VehicleInfoController::class, 'show']);
+
+Route::post('/api/conexion_chatgpt', [ChatGptController::class, 'handle'])
+    ->withoutMiddleware([
+        Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+    ]);
