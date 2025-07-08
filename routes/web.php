84f5a74adb29ced4f2c6f312951dfa86a405b3d9
danielmmerino <danielm_merino@hotmail.com>
2025-07-07 22:05:31 -0500

    Add cotizar salud endpoint

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\VehicleInfoController;
 use App\Http\Controllers\ChatGptController;
 use App\Http\Controllers\CommercialInfoController;
 use App\Http\Controllers\PreferenciasSaludController;
+use App\Http\Controllers\CotizarSaludPorPreferenciasController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -29,6 +30,11 @@ Route::get('/api/comercial-info', [CommercialInfoController::class, 'show'])
     ]);
 
 Route::get('/api/opciones_preferencias_salud', [PreferenciasSaludController::class, 'index'])
+    ->withoutMiddleware([
+        Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+    ]);
+
+Route::post('/api/cotizar_salud_por_preferencias', [CotizarSaludPorPreferenciasController::class, 'handle'])
     ->withoutMiddleware([
         Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
     ]);
